4691e1264e4c8d095bd3c6e26e2dc99b5a6df88a
Arunachalam_RM <Arunachalam_RM@outlook.com>
2025-05-03 12:05:53 +0530

    Add project files.

---
+++ Fields/Product.cs
@@ -0,0 +1,55 @@
+﻿namespace Fields
+{
+    public class Product
+    {
+        public static string mode = "Online";//static field
+        public int id; //instance fields
+        public readonly string dateOfPurchase;//readonly instance field
+        public string name;
+        public double cost;
+        public int quantityInStock;
+        private int _productIMEI;//private field
+        protected int prodWarranty;
+        internal bool availability;
+        protected internal string country;
+        public const int prodTaxNumber = 100;//const field
+        public double tax;
+
+        public Product() { 
+            dateOfPurchase = DateTime.Now.ToShortDateString();//MM-DD-YYYY
+        }
+        public Product(string dateOfPurch)
+        {
+            dateOfPurchase=dateOfPurch;//initailizing readonly field with constructor parameter
+        }
+        public void Mode()
+        {
+            System.Console.WriteLine(mode);//no need of classname also when accessing static field
+        }
+    }
+
+    public class SubProduct : Product
+    {
+        public void Method()
+        {
+            System.Console.WriteLine(id);//can access public field
+            System.Console.WriteLine(prodWarranty);//can access protected field
+            System.Console.WriteLine(availability);//can access internal field
+            System.Console.WriteLine(country);//can access protected internal  field
+            //System.Console.WriteLine(_productIMEI);//can't access the private field in other classes
+        }
+    }
+
+    public class OtherClassSameAssembly
+    {
+        public void Method()
+        {
+            Product lp=new Product();//can't prevent private field being stored inside object 
+            System.Console.WriteLine(lp.id);//can access public field in other classes
+            // System.Console.WriteLine(lp.prodWarranty); //can't access the protected field in other than child classes
+            System.Console.WriteLine(lp.availability);//can access internal field in other classes
+            //System.Console.WriteLine(lp._productIMEI);//can't access the private field in other classes
+            System.Console.WriteLine(lp.country);//can access protected internal  field
+        }
+    }
+}
---
+++ FieldsDemo/ProductConsole.cs
@@ -0,0 +1,98 @@
+﻿using Fields;
+
+class Sample
+{
+    public static void Main(string [] args)
+    {
+        Product laptop = new Product();
+        laptop.cost = 80000.78;
+        laptop.id = 176809;
+        laptop.name = "hp 15";
+        laptop.quantityInStock = 3;
+        //laptop._productIMEI  //can't access the private field in other classes
+        Console.WriteLine("Static field->"+Product.mode);//Online //accessing static field with classname
+        Console.WriteLine("constant field" + Product.prodTaxNumber);//100 accessing constant field
+        Console.WriteLine("laptop dop:"+laptop.dateOfPurchase);//28-11-2024 //accessing readonly field
+        
+        Product mobile = new Product("18-07-1999");
+        mobile.cost = 26000.8;
+        mobile.id = 1709;
+        mobile.name = "oppo 65";
+        mobile.quantityInStock = 2;
+        Console.WriteLine("mobile dop:" + mobile.dateOfPurchase);//18-07-1999 //accessing readonly field
+
+        Product book = new Product();
+        book.cost = 900.8;
+        book.id = 1709;
+        book.name = "pooniyin selvan";
+        book.quantityInStock = 1;
+
+        System.Console.WriteLine("laptop cost:"+laptop.cost);//80000.78
+        System.Console.WriteLine("lap QIS:"+laptop.quantityInStock);//3
+
+        double totalcost = book.cost + laptop.cost + mobile.cost;
+        System.Console.WriteLine("total cost :"+totalcost);//106902.38
+        System.Console.WriteLine("highest product cost : "+HighestProductCost(book,laptop,mobile));//80000.78
+
+        LocalConstantsDemo();
+    }
+
+    private static void LocalConstantsDemo()
+    {
+        const double pi = 3.14;//local constant
+        System.Console.WriteLine(pi);//3.14
+    }
+
+    public class InternationalProduct : Product
+    {
+        public void Method()
+        {
+            System.Console.WriteLine(id);//accessing public field
+            System.Console.WriteLine(prodWarranty);//accessing protected field
+            //System.Console.WriteLine(availability);//can't access the internal field in other assemblies
+            //System.Console.WriteLine(_productIMEI);//can't access the private field in other classes
+            System.Console.WriteLine(country);//can access protected internal  field in child classes
+        }
+    }
+
+
+    public class OtherClassOtherAssembly
+    {
+        public void Method()
+        {
+            Product lp = new Product();//can't prevent private field being stored inside object 
+            System.Console.WriteLine(lp.id);
+            // System.Console.WriteLine(lp.prodWarranty); //can't access the protected field in other than child classes
+            // System.Console.WriteLine(lp.availability);//can't access the internal field in other assemblies
+            //System.Console.WriteLine(lp._productIMEI);//can't access the private field in other classes
+            //System.Console.WriteLine(lp.country);//can't access protected internal  field in other classes
+        }
+    }
+
+    /// <summary>
+    /// Find Highest product cost
+    /// </summary>
+    /// <param name="p1"></param>
+    /// <param name="p2"></param>
+    /// <param name="p3"></param>
+    /// <returns>double Cost</returns>
+    public static double HighestProductCost(Product p1, Product p2, Product p3)
+    {
+        double highest;
+        if (p1.cost >= p2.cost)
+        {
+            if (p1.cost >= p3.cost)
+                highest = p1.cost;
+            else
+                highest = p3.cost;
+        }
+        else
+        {
+            if (p2.cost >= p3.cost)
+                highest = p2.cost;
+            else
+                highest = p3.cost;
+        }
+        return highest;
+    }
+}
